3d468df8b9cf04841fcd614886d186de8cc457ea
hapizo3 <necosan2019.program@gmail.com>
2019-10-17 15:33:35 +0900

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject teleBall;
+
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetMouseButtonDown( 0 ) )
+		{
+			Vector3 tmp = GameObject.FindGameObjectWithTag( "Player" ).transform.position;
+			Instantiate( teleBall, new Vector3( tmp.x, tmp.y, tmp.z ), Quaternion.identity );
+		}
+	}
+}
---
+++ Assets/Hapizo/teleBallCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleBallCollision : MonoBehaviour {
+
+	public GameObject teleportParticle;
+
+	
+
+	void OnCollisionEnter( Collision other )
+	{
+		float x = this.gameObject.transform.position.x;
+		float y = this.gameObject.transform.position.y;
+		float z = this.gameObject.transform.position.z;
+		Quaternion q = Quaternion.Euler( -90f, 0f, 0f );
+		if( other.gameObject.tag == "Ground" )
+		{
+
+			Instantiate( teleportParticle, new Vector3( x, y, z ), Quaternion.identity * q );
+			Destroy( this.gameObject );
+		}
+	}
+}
