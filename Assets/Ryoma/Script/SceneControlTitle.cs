e9c116eaecd6699bce072c3d0054ede65b32bb64
ellio001 <11806042@stu.kbc.ac.jp>
2019-10-21 08:50:49 +0900

    ScenContloreまだ未確定なので、メインができたら声掛けお願いします。

--- Assets/Ryoma/Script/SceneControlTitle.cs
+++ Assets/Ryoma/Script/SceneControlTitle.cs
@@ -11,6 +11,6 @@ public class SceneControlTitle : MonoBehaviour {
     // 3.OnRetry関数が実行されたら、sceneを読み込む
     public void OnRetry() {
         // 「ButtonScene」を自分の読み込みたいscene名に変える
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Main2");
     }
 }
