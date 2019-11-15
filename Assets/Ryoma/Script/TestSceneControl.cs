c1bfd7d1ae7b6486d32913a5f2810a93a6d2de52
ellio001 <11806042@stu.kbc.ac.jp>
2019-11-15 13:07:13 +0900

    音直し中

---
+++ Assets/Ryoma/Script/TestSceneControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TestSceneControl : MonoBehaviour {
+
+    private new AudioSource audio;
+    void Start() {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+
+        StartCoroutine(Checking(() => {
+            Debug.Log("END");
+        }));
+    }
+
+    public delegate void functionType();
+    public IEnumerator Checking(functionType callback) {
+        while (true) {
+            yield return new WaitForFixedUpdate();
+            if (!audio.isPlaying) {
+                callback();
+                OnRetry();
+                break;
+            }
+        }
+    }
+
+
+    // 3.OnRetry関数が実行されたら、sceneを読み込む
+    public void OnRetry() {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Menu");
+    }
+}
