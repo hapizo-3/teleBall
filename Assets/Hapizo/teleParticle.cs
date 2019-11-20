319270ce7b93d89b9854742edc98b6cb753f3c00
hapizo3 <necosan2019.program@gmail.com>
2019-11-20 15:37:09 +0900

    1

---
+++ Assets/Hapizo/teleParticle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleParticle : MonoBehaviour {
+
+	private int occureTime = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		occureTime++;
+		if( occureTime >= 180 )
+		{
+			Destroy( this.gameObject );
+		}
+	}
+}
