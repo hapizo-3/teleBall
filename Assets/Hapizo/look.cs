00cc736421ebee37a66dc64ec408b89e0963aad4
hapizo3 <necosan2019.program@gmail.com>
2019-11-20 17:37:26 +0900

    とりあえず直っただけあげときます P.S. 視野固定できてないので今から頑張ります。きりくん、頑張ってください。

--- Assets/Hapizo/look.cs
+++ Assets/Hapizo/look.cs
@@ -7,20 +7,28 @@ public class look : MonoBehaviour {
 	public Transform verRot;
 	public Transform horRot;
 	public float sensitivity = 0.1f;
+	public GameObject GManagerObject;
+	GameManager gManager;
 
 	// Use this for initialization
 	void Start()
 	{
 		verRot = transform.parent;
 		horRot = GetComponent<Transform>();
+		GManagerObject = GameObject.FindGameObjectWithTag( "GameManager" );
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		float X_Rotation = Input.GetAxis( "Mouse X" ) * sensitivity;
-		float Y_Rotation = Input.GetAxis( "Mouse Y" ) * sensitivity;
-		verRot.transform.Rotate( 0, -X_Rotation, 0 );
-		horRot.transform.Rotate( Y_Rotation, 0, 0 );
+		gManager = GManagerObject.GetComponent<GameManager>();
+
+		if( gManager.wasLocked == true )
+		{
+			float X_Rotation = Input.GetAxis( "Mouse X" ) * sensitivity;
+			float Y_Rotation = Input.GetAxis( "Mouse Y" ) * sensitivity;
+			verRot.transform.Rotate( 0, -X_Rotation, 0 );
+			horRot.transform.Rotate( Y_Rotation, 0, 0 );
+		}
 	}
 }
