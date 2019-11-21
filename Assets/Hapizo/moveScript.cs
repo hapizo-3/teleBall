28648010ec01abe01c51680b7399c23b60265e14
hapizo3 <necosan2019.program@gmail.com>
2019-11-21 15:00:25 +0900

    update quaternion test

--- Assets/Hapizo/moveScript.cs
+++ Assets/Hapizo/moveScript.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class moveScript : MonoBehaviour {
 
+	public Vector3 forward;
+	public Quaternion q;
+
 	// Use this for initialization
 	void Start () {
-		
+		forward = transform.forward;
+		q = this.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log( "x/" + forward.x + " , y/" + forward.y + " ,z/" + forward.z 
+						+ "\n               ,qx/" + q.x + " ,qy/" + q.y + " ,qz/" + q.z );
 		this.gameObject.transform.position += this.gameObject.transform.forward * Time.deltaTime;
+		//this.gameObject.transform.position += Vector3.forward * Time.deltaTime;
 	}
 }
