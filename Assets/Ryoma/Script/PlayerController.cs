a0fe2e4986e7399cd158ff956dbfc3ad3560a1b6
ellio001 <11806042@stu.kbc.ac.jp>
2019-11-25 17:35:56 +0900

    Stege04 Finishi

---
+++ Assets/Ryoma/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 2.0f;
+    private Rigidbody rB;
+
+	// Use this for initialization
+	void Start () {
+        rB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rB.AddForce(x * speed, 0, z * speed, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Ryoma/Script/RideOnOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RideOnOff : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        GameObject emptyObject = new GameObject();
+        emptyObject.transform.parent = this.transform;
+        other.transform.parent = this.transform;
+        emptyObject.name = "empty";
+        Debug.Log("乗った");
+    }
+
+    private void OnTriggerExit(Collider other) {
+        other.transform.parent = null;
+        GameObject emptyObject = GameObject.Find("empty");
+        Destroy(emptyObject);
+    }
+}
