7ae6565853692bb44201779a3bcb5353761efd0f
libra21 <11806015@stu.kbc.ac.jp>
2019-12-02 10:35:10 +0900

    クリックしたらアニメーション

---
+++ Assets/Maiko/Throw.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = this.gameObject.GetComponent<Animator>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.animator.SetTrigger("ThrowTrigger");
+        }
+	}
+}
