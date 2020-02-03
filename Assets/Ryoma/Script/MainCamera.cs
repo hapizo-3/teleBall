14f341223c39a09f1f4725031f9f11441a718268
ellio001 <11806042@stu.kbc.ac.jp>
2020-02-03 13:06:51 +0900

    最終

--- Assets/Ryoma/Script/MainCamera.cs
+++ Assets/Ryoma/Script/MainCamera.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour {
 
+    //ステージを移動させるフラグ
+    int Fag = 0;
+    
+    [SerializeField] float initAnglesY = 0f;
+    
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        gameObject.transform.eulerAngles += new Vector3(0f, initAnglesY, 0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.eulerAngles += new Vector3(0f, 0.1f, 0f);
+        gameObject.transform.eulerAngles += new Vector3(0f, 0.5f, 0f);
+        if (this.gameObject.transform.eulerAngles.y >= 350.0f ){
+            Debug.Log(this.gameObject.transform.eulerAngles.y);
+            this.gameObject.transform.eulerAngles = new Vector3(0f, initAnglesY, 0f);
 
+        }
 	}
 }
